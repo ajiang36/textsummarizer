a458d5906df3cee5eea46074783cb43d8ff046a2
iRove108 <iRove108@users.noreply.github.com>
2020-10-15 15:24:00 -0500

    Expand comments in nlbasics.py with more detailed task descriptions

--- textsummarizer/nlbasics.py
+++ textsummarizer/nlbasics.py
@@ -7,7 +7,7 @@ nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 
-# Tokenizing text
+# Tokenizing text, i.e. break up text into lists of words/sentences
 text = input ("Enter your text to tokenize here: ")
 from nltk.tokenize import word_tokenize, sent_tokenize
 
@@ -17,7 +17,7 @@ print(sents)
 words=[word_tokenize(sent) for sent in sents]
 print(words)
 
-# Removing stopwords
+# Removing stopwords, commonly used words like "the", "and", etc.
 from nltk.corpus import stopwords
 from string import punctuation
 
@@ -25,24 +25,30 @@ customStopWords=set(stopwords.words('english')+list(punctuation))
 wordsWOStopwords=[word for word in word_tokenize(text) if word not in customStopWords]
 print(wordsWOStopwords)
 
-#Identifying bigrams
+# Identifying bigrams, groupings of two consecutive words
 from nltk.collocations import *
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 finder = BigramCollocationFinder.from_words(wordsWOStopwords)
 print(finder.ngram_fd.items())
 
-# Stemming
+# Stemming (extract stem of word, e.g. the stem of swimming is swim)
 text2 = input ("Enter your text to stem and identify POS here: ")
 from nltk.stem.lancaster import LancasterStemmer
 st = LancasterStemmer()
 stemmedWords = [st.stem(word) for word in word_tokenize(text2)]
 print(stemmedWords)
 
-# POS Tagging
+# POS (part of speech) tagging
 tagList = nltk.pos_tag(word_tokenize(text2))
 pprint(tagList)
 
 # Disambiguating word meanings
+# Words can mean different things depending on their context, and lesk
+# uses a word's context to determine which definition of the word
+# is being used.
+# You can find a description of the algorithm at
+# http://www.nltk.org/howto/wsd.html
+
 word = input ("Enter your word here: ")
 from nltk.corpus import wordnet as wn
 
